d0141ccb67b8f34f5031628eacc163949c1c76e8
Lorcan BRENDERS <lorcan.brenders@etu.univ-lyon1.fr>
2025-07-23 17:13:34 +0200

    changing réunion to calendar

--- src/components/calendarGarde.vue
+++ src/components/calendarGarde.vue
@@ -12,7 +12,7 @@
                 :key="index"
                 :class="['calendar-day', { 'past-day': day.isPast }]"
             >
-                <div :class="['day-number', day.isPast ? "" : day.equipeGarde]">{{ day.date.getDate() }}</div>
+                <div :class="['day-number', day.isPast ? null : day.equipeGarde]">{{ day.date.getDate() }}</div>
             </div>
         </div>
     </div>
